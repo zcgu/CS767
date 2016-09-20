e5d32e1c9ac5e28e6e14bd2a18a9d729c1b90e52
Zhicheng Gu <zchenggu@gmail.com>
2016-09-19 22:02:54 -0500

    p3 part1

---
+++ Assign1/test-image/p3/p3_part1.m
@@ -0,0 +1,43 @@
+% clear
+% a = imread('img_ct_lung.jpg');
+a = imread('edge_detector.png');
+
+% imshow(a)
+a = im2double(a);
+subplot(2, 3, 1);
+imagesc(a);
+
+p = 1.0;
+
+h = fspecial( 'gaussian', 5, p );
+
+b = conv2(a, h, 'same');
+subplot(2, 3, 2);
+imagesc(b);
+
+gx = [-1, 0, 1; -2, 0, 2; -1, 0, 1];
+gy = [-1, -2, -1; 0, 0, 0; 1, 2, 1];
+
+bx = conv2(b, gx, 'same');
+by = conv2(b, gy, 'same');
+
+g = zeros(size(bx));
+t = zeros(size(bx));
+
+[m ,n] = size(bx);
+
+for i = 1:m
+    for j =1:n
+        g(i, j) = (bx(i, j) ^ 2 + by(i, j) ^ 2) ^ 0.5;
+        t(i, j) = atan(by(i, j) / bx(i, j));
+    end
+end
+
+subplot(2, 3, 3);
+imagesc(g);
+subplot(2, 3, 4);
+
+
+quiver(sin(t), cos(t));
+
+        
